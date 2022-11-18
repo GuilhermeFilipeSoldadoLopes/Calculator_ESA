8247481ca54874d27a71188173c9e4b2f3bce570
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2022-11-18 10:01:11 +0000

    Calculator_ESA

--- main.py
+++ main.py
@@ -2,17 +2,30 @@
 def add(x, y):
     return x + y
 
+
 # This function subtracts two numbers
 def subtract(x, y):
     return x - y
+
 
 # This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
+
 # This function divides two numbers
 def divide(x, y):
     return x / y
+
+
+# This function verify if s is a number
+def isNotNumber(s):
+    try:
+        float(s)
+        return False
+    except ValueError:
+        return True
+
 
 print("Select operation.")
 print("1.Add")
@@ -26,8 +39,19 @@ while True:
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+
+        num1 = input("Enter first number: ")
+        while isNotNumber(num1):
+            print("- Invalid Input, try again")
+            num1 = input("Enter first number: ")
+
+        num1 = float(num1)
+        num2 = input("Enter second number: ")
+        while isNotNumber(num2):
+            print("- Invalid Input, try again")
+            num2 = input("Enter second number: ")
+
+        num2 = float(num2)
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
@@ -43,13 +67,21 @@ while True:
 
         # check if user wants another calculation
         # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        next_calculation = next_calculation.lower()
-        if next_calculation == "no" or next_calculation == "0" or next_calculation == "n":
-            print("Closing Program")
+        leave = True
+        while leave:
+            next_calculation = input("Let's do next calculation? (yes/no): ")
+            next_calculation = next_calculation.lower()
+            if next_calculation == "no" or next_calculation == "n":
+                break
+            if next_calculation == "yes" or next_calculation == "y":
+                leave = False
+            else:
+                print("- Invalid Input")
+
+        if leave:
+            print("- Closing Program")
             break
-        if next_calculation == "yes" or next_calculation == "1" or next_calculation == "y"
-            
+
 
     else:
-        print("Invalid Input")
+        print("- Invalid Input")

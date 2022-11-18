880f1e3566f2317facabe70eec37c8a5c244b192
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2022-11-18 10:34:03 +0000

    Calculator_ESA

--- main.py
+++ main.py
@@ -59,16 +59,16 @@ while True:
         num2 = float(num2)
 
         if choice == '1':
-            print(num1, "+", num2, " = ", add(num1, num2))
+            print(num1, "+", num2, "=", add(num1, num2))
 
         elif choice == '2':
-            print(num1, "-", num2, " = ", subtract(num1, num2))
+            print(num1, "-", num2, "=", subtract(num1, num2))
 
         elif choice == '3':
-            print(num1, "*", num2, " = ", multiply(num1, num2))
+            print(num1, "*", num2, "=", multiply(num1, num2))
 
         elif choice == '4':
-            print(num1, "/", num2, " = ", divide(num1, num2))
+            print(num1, "/", num2, "=", divide(num1, num2))
 
         elif choice == '5':
             print(num1, "/", num2, "* 100" " =", percentage(num1, num2),"%")

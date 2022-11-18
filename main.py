30a81127f5f7b975dd816f4e953fee3786e29a3e
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2022-11-18 10:17:44 +0000

    Calculator_ESA

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def divide(x, y):
     return x / y
 
 
+# This function return a percentage
+def percentage(x, y):
+    return divide(x, y) * 100
+
+
 # This function verify if s is a number
 def isNotNumber(s):
     try:
@@ -35,7 +40,7 @@ print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
@@ -54,16 +59,19 @@ while True:
         num2 = float(num2)
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(num1, "+", num2, " = ", add(num1, num2))
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(num1, "-", num2, " = ", subtract(num1, num2))
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(num1, "*", num2, " = ", multiply(num1, num2))
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            print(num1, "/", num2, " = ", divide(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "*100" " = ", percentage(num1, num2), "%")
 
         # check if user wants another calculation
         # break the while loop if answer is no
